f5921e7d87a89263d215693c8e3b1d72bc061a35
Hugo Bock <hugobock.dev@gmail.com>
2026-09-12 12:54:20 +0200

    Normalise engine registry

--- apps/psy_trainer/lib/features/train/presentation/engine/engine_registry_provider.dart
+++ apps/psy_trainer/lib/features/train/presentation/engine/engine_registry_provider.dart
@@ -4,10 +4,10 @@ import '../../../engines/arithmetic_grid/domain/arithmetic_grid_engine.dart';
 import '../../../engines/arithmetic_grid/presentation/arithmetic_grid_renderer.dart';
 import '../../../engines/attention_parity/domain/attention_parity_engine.dart';
 import '../../../engines/attention_parity/presentation/attention_parity_renderer.dart';
-import '../../../engines/logic_dominos/domain/dominos_engine.dart';
-import '../../../engines/logic_dominos/presentation/dominos_renderer.dart';
 import '../../../engines/attention_rules/domain/attention_rules_engine.dart';
 import '../../../engines/attention_rules/presentation/attention_rules_renderer.dart';
+import '../../../engines/logic_dominos/domain/dominos_engine.dart';
+import '../../../engines/logic_dominos/presentation/dominos_renderer.dart';
 import '../../domain/engine/engine.dart';
 import 'activity_renderer.dart';
 
@@ -23,14 +23,11 @@ import 'activity_renderer.dart';
 final Provider<EngineRegistry> engineRegistryProvider =
     Provider<EngineRegistry>(
       (ref) => EngineRegistry(const <ActivityEngine>[
-        // US-021..036: add engines here, one line each (alphabetical by
-        // family id).
-        ArithmeticGridEngine(),
-        // US-021..036: add engines here, one line each.
-        DominosEngine(),
-        AttentionParityEngine(),
-        AttentionRulesEngine(),
-        // US-021..036: add engines here, one line each.
+    // US-021..036: add one line per engine (alphabetical by family id).
+    ArithmeticGridEngine(),
+    AttentionParityEngine(),
+    AttentionRulesEngine(),
+    DominosEngine(),
       ]),
     );
 
@@ -38,14 +35,11 @@ final Provider<EngineRegistry> engineRegistryProvider =
 final Provider<RendererRegistry> rendererRegistryProvider =
     Provider<RendererRegistry>(
       (ref) => RendererRegistry(const <ActivityRenderer>[
-        // US-021..036: add renderers here, one line each (alphabetical by
-        // family id).
-        ArithmeticGridRenderer(),
-        // US-021..036: add renderers here, one line each.
-        DominosRenderer(),
-        AttentionParityRenderer(),
-        AttentionRulesRenderer(),
-        // US-021..036: add renderers here, one line each.
+    // US-021..036: add one line per engine (alphabetical by family id).
+    ArithmeticGridRenderer(),
+    AttentionParityRenderer(),
+    AttentionRulesRenderer(),
+    DominosRenderer(),
       ]),
     );
 
