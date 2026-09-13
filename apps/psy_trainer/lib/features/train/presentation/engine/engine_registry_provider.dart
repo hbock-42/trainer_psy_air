7475edc142443a5d9add03715173b86dec00a213
Hugo Bock <hugobock.dev@gmail.com>
2026-09-13 02:01:52 +0200

    Normalise engine registry

--- apps/psy_trainer/lib/features/train/presentation/engine/engine_registry_provider.dart
+++ apps/psy_trainer/lib/features/train/presentation/engine/engine_registry_provider.dart
@@ -20,14 +20,14 @@ import '../../../engines/multitask_psychomotor/domain/multitask_engine.dart';
 import '../../../engines/multitask_psychomotor/presentation/multitask_renderer.dart';
 import '../../../engines/p1_angles/domain/p1_angles_engine.dart';
 import '../../../engines/p1_angles/presentation/p1_angles_renderer.dart';
-import '../../../engines/p1_wm_calc_back/domain/calc_back_engine.dart';
-import '../../../engines/p1_wm_calc_back/presentation/calc_back_renderer.dart';
-import '../../../engines/p1_wm_reverse_span/domain/reverse_span_engine.dart';
-import '../../../engines/p1_wm_reverse_span/presentation/reverse_span_renderer.dart';
 import '../../../engines/p1_counters/domain/p1_counters_engine.dart';
 import '../../../engines/p1_counters/presentation/p1_counters_renderer.dart';
 import '../../../engines/p1_mental_arithmetic/domain/mental_arithmetic_engine.dart';
 import '../../../engines/p1_mental_arithmetic/presentation/mental_arithmetic_renderer.dart';
+import '../../../engines/p1_wm_calc_back/domain/calc_back_engine.dart';
+import '../../../engines/p1_wm_calc_back/presentation/calc_back_renderer.dart';
+import '../../../engines/p1_wm_reverse_span/domain/reverse_span_engine.dart';
+import '../../../engines/p1_wm_reverse_span/presentation/reverse_span_renderer.dart';
 import '../../../engines/planning_tubes/domain/tubes_engine.dart';
 import '../../../engines/planning_tubes/presentation/tubes_renderer.dart';
 import '../../../engines/spatial_cubes/domain/cube_net_engine.dart';
@@ -60,6 +60,7 @@ final Provider<EngineRegistry> engineRegistryProvider =
         const ArithmeticGridEngine(),
         const AttentionParityEngine(),
         const AttentionRulesEngine(),
+        const CalcBackEngine(),
         const CountersEngine(),
         const CubeNetEngine(),
         const CultureAeroEngine(),
@@ -70,7 +71,6 @@ final Provider<EngineRegistry> engineRegistryProvider =
         const NbackEngine(),
         const OverlayGridEngine(),
         const P1AnglesEngine(),
-        const CalcBackEngine(),
         const ReverseSpanEngine(),
         const TubesEngine(),
         const ViewpointEngine(),
@@ -87,6 +87,7 @@ final Provider<RendererRegistry> rendererRegistryProvider =
         const ArithmeticGridRenderer(),
         const AttentionParityRenderer(),
         const AttentionRulesRenderer(),
+        const CalcBackRenderer(),
         const CountersRenderer(),
         const CubeNetRenderer(),
         const DominosRenderer(),
@@ -104,7 +105,6 @@ final Provider<RendererRegistry> rendererRegistryProvider =
         const NbackRenderer(),
         const OverlayGridRenderer(),
         const P1AnglesRenderer(),
-        const CalcBackRenderer(),
         const ReverseSpanRenderer(),
         const TubesRenderer(),
         const ViewpointRenderer(),
